b4cccc2635435682e43a7ac24d3cbfddbf0040b2
Dynamite <mdejaz4450@gmail.com>
2021-02-04 13:27:11 +0530

    https://github.com/mdejaz4450/100-days-of-code-challenge/blob/main/README.md

---
+++ singlyLinkedList.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class SinglyLinkedList{
+    public:
+    int data;
+    SinglyLinkedList* Next; // Next refrence of node of class type.    
+};
+
+ void PrintList(SinglyLinkedList* head){
+     printf("WELCOME TO THE 100 DAYS OF CODE CHALLENGE!\n");
+
+     while (head != NULL)
+     {
+         cout<<head->data<<" ";
+         head = head->Next;
+     }
+           
+}
+
+int main(){
+    SinglyLinkedList* head = NULL;
+    SinglyLinkedList* second = NULL;
+    SinglyLinkedList* third = NULL;
+
+
+    // allocate memory using new keyword in C++.
+    head = new SinglyLinkedList();
+    second = new SinglyLinkedList();
+    third = new SinglyLinkedList();
+
+
+    //Dynamic Memory allocation C using malloc.
+   // third = (SinglyLinkedList *)malloc(sizeof(SinglyLinkedList*));
+
+   head->data = 8;
+   head-> Next = second;
+
+   second ->data = 7;
+   second ->Next = third;
+
+   third ->data = 4;
+   third ->Next = NULL; // Here linked list is terminated.
+
+   PrintList(head);
+   return 0;
+}
+
