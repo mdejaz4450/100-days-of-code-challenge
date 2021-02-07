c5481ef7276edf6b63f50799894f58981cbec767
Dynamite <mdejaz4450@gmail.com>
2021-02-07 11:49:24 +0530

    init

---
+++ LinkedList_01.cpp
@@ -0,0 +1,35 @@
+// SINGLY LINKED LIST (: Insertion Operation
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+
+};
+
+//Display list
+void Traverse(){
+
+}
+
+// Insert at first index;
+struct Node* AtFirst(){
+
+}
+
+// Insert at between any node.
+struct Node* AtBetween(){
+
+}
+
+// Insert at the end. 
+struct Node* AtEnd(){
+
+}
+
+int main(){
+    cout<<" Hello World!";
+    return 0;
+}
