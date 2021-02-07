0573998152903323cf296d59c6c1219d751b076f
Dynamite <mdejaz4450@gmail.com>
2021-02-07 17:16:39 +0530

    inti

--- LinkedList_01.cpp
+++ LinkedList_01.cpp
@@ -1,35 +1,78 @@
-// SINGLY LINKED LIST (: Insertion Operation
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 struct Node
 {
     int data;
-    Node* next;
-
+    Node *next;
 };
 
 //Display list
-void Traverse(){
-
+void Traverse(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        cout << ptr->data << " -> ";
+        ptr = ptr->next;
+    }
+    cout << "->NULL";
 }
 
 // Insert at first index;
-struct Node* AtFirst(){
-
+struct Node *AtFirst(struct Node *start, int value)
+{
+    struct Node *newNode;
+    newNode = (struct Node *)malloc(sizeof(struct Node *));
+    newNode->data = value;
+    newNode->next = start;
+    return newNode;
 }
 
 // Insert at between any node.
-struct Node* AtBetween(){
+struct Node *AtBetween(struct Node *start, int data, int index)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
+    struct Node *ptr = start;
 
+    int i = 0;
+    while (i != index - 1)
+    {
+        ptr = ptr->next;
+        i++;
+    }
+
+    newNode->data = data;
+    newNode->next = ptr->next;
+    ptr->next = newNode;
+    return start;
 }
 
-// Insert at the end. 
-struct Node* AtEnd(){
+// Insert at the end.
+struct Node *AtEnd(struct Node *start, int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
+    newNode->data = data;
 
+    struct Node *ptr = start;
+
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    newNode->next = NULL;
+    ptr->next = newNode;
+    return start;
 }
 
-int main(){
-    cout<<" Hello World!";
+int main()
+{
+    struct Node *start;
+    start = (struct Node *)malloc(sizeof(struct Node *));
+    start = AtFirst(start, 9);
+    start = AtFirst(start, 9);
+    start = AtBetween(start,10,2);
+    start = AtEnd(start,15);
+    Traverse(start);
     return 0;
 }
