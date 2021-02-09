ec9618cfc99635d8282570b5a7f75e9343d23f51
Dynamite <mdejaz4450@gmail.com>
2021-02-09 12:58:52 +0530

    init

--- LinkedList_04.cpp
+++ LinkedList_04.cpp
@@ -1,6 +1,56 @@
+#include<stdio.h>
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
+
+//DOUBLY LINKED LIST:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+void linkedListTraversal(struct Node *head)
+{
+    while(head != NULL){
+        cout<<head->data<<" ->";
+        head = head->next;
+    }
+    cout<<" NULL"<<endl;
+}
+
 int main(){
-    cout<<"Hello";
+    
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 4;
+    head->next = second;
+    head -> prev = NULL;
+
+    second->data = 3;
+    second->next = third;
+    second -> prev = head;
+
+    third->data = 6;
+    third->next = fourth;
+    third -> prev = second;
+
+    fourth->data = 1;
+    fourth->next = NULL;
+    fourth ->prev = third;
+
+
+    linkedListTraversal(head);
     return 0;
 }
